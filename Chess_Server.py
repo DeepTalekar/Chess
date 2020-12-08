9eb7375cbec674c61dcb296deee149ae3d965513
Deep Talekar <56110201+DeepTalekar@users.noreply.github.com>
2020-12-09 02:06:25 +0530

    Created a Server for the Chess Client.py
    It just echo's the data that is received back to the client again.
    And it is a Concurrent Server.

---
+++ Chess_Server.py
@@ -0,0 +1,56 @@
+import socket
+import threading
+
+print("Starting the Chess Server!!")
+
+soc = socket.socket()
+
+host = 'localhost'
+port = 8091
+
+clist = []
+
+class Move (threading.Thread):
+    def __init__(self, client, address):
+        threading.Thread.__init__(self)
+        self.client = client
+        self.add = address
+    
+    def run(self):
+        global clist
+        clist.append(self.client)
+
+        stringDict = self.client.recv(1024)
+        stringDict = stringDict.decode()
+        if stringDict != 'q':
+            self.moveMade(stringDict)
+
+        while stringDict != 'q':
+            [soc.send(stringDict.encode()) for soc in clist]
+            print("The data recieved from the Client with address {} is --> {}".format(self.add, stringDict))
+            stringDict = self.client.recv(1024)
+            stringDict = stringDict.decode()
+            if stringDict != 'q':
+                self.moveMade(stringDict)
+
+        print("The Client with the address {} has been Disconnected".format(self.add))
+        self.client.send("You just quitted the Game".encode())
+        clist.remove(self.client)
+        self.client.close()
+
+    def moveMade(self,stringDict):
+       moveDict = eval(stringDict)
+       print("Just Converted the String to Dictionary again --> ", moveDict)
+
+soc.bind((host, port))
+
+soc.listen(5)
+
+while True:
+    print("| Waiting for the Client |")
+    client, address = soc.accept()
+    Move(client, address).start()
+    
+# Generally we want to keep the Server Running so below statements are useless but its just for the practice
+soc.close()
+print("Chess Server Shutting Down!!")
